2ea37cf388b3f14b15808187b410321b77581359
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-07 12:40:52 +0200

    Initial commit

---
+++ src/main/java/org/neuro/Main.java
@@ -0,0 +1,14 @@
+package org.neuro;
+
+/**
+ * ${NAME}.
+ *
+ * @author ${USER}
+ * @version 5.0
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
